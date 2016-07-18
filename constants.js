bc7650eb0aab6f1c27bc89713147311c92f7dd20
Brad Parker <hi@bradparker.com>
2016-07-18 15:03:12 +1000

    Better prepare options from env vars

--- constants.js
+++ constants.js
@@ -2,28 +2,37 @@ const { join } = require('path')
 
 const PROD = process.env.NODE_ENV === 'production'
 
-const INPUT_DIR = process.env.INPUT_DIR || join(process.cwd(), 'source')
+const INPUT_DIR = join(process.cwd(), process.env.INPUT_DIR || 'source')
 
 const stringToArray = (str = '') => (
   str.split(',').map((elem) => elem.trim())
 )
 
+const trimPath = (path = '') => (
+  path.replace().replace(/^\/|\/$/g, '')
+)
+
+const ensureAbsolute = (path = '') => {
+  if (!path) return '/'
+  return `/${path}/`
+}
+
 const CLIENT_ENTRIES = stringToArray(process.env.CLIENT_ENTRIES || './client.js')
 const SERVER_ENTRIES = stringToArray(process.env.SERVER_ENTRIES || './server.js')
 
-const OUTPUT_DIR = process.env.OUTPUT_DIR || join(process.cwd(), 'dist')
-const CLIENT_OUTPUT_DIR = process.env.CLIENT_OUTPUT_DIR || OUTPUT_DIR
-const SERVER_OUTPUT_DIR = process.env.SERVER_OUTPUT_DIR || OUTPUT_DIR
+const DEFAULT_OUTPUT_DIR = './dist'
+const CLIENT_OUTPUT_DIR = join(process.cwd(), process.env.CLIENT_OUTPUT_DIR || DEFAULT_OUTPUT_DIR)
+const SERVER_OUTPUT_DIR = join(process.cwd(), process.env.SERVER_OUTPUT_DIR || process.env.CLIENT_OUTPUT_DIR || DEFAULT_OUTPUT_DIR)
 
 const BASE_URL = process.env.BASE_URL || ''
-const BASE_PATH = process.env.BASE_PATH || ''
-const ASSET_PATH = process.env.ASSET_PATH || ''
+const BASE_PATH = trimPath(process.env.BASE_PATH || '')
+const ASSET_PATH = ensureAbsolute(trimPath(process.env.ASSET_PATH || ''))
 
 const PUBLIC_PATH = [
   BASE_URL,
   BASE_PATH,
   ASSET_PATH
-].filter((p) => !!p).join('') + '/'
+].filter((p) => !!p).join('')
 
 module.exports = {
   PROD,
