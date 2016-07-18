b2019996e5ce8119406191258d9b28833172468a
Brad Parker <hi@bradparker.com>
2016-07-18 12:11:36 +1000

    Flesh out basic example

--- examples/basic/source/server.js
+++ examples/basic/source/server.js
@@ -1,1 +1,15 @@
-export const renderDocument = () => 'It\'s alive'
+export default (assets) => {
+  const renderDocument = (name) => ([
+    'Hello, ' + name,
+    'Assets: ', Object.keys(assets).join(', ')
+  ].join('\n'))
+
+  const app = (name) => Promise.resolve(
+    renderDocument(name)
+  )
+
+  return {
+    app,
+    renderDocument
+  }
+}
