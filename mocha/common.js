c4c145dfd0cb631bce2a514d4c88d6d7225b0f02
Sam Enoka <slwenoka@gmail.com>
2016-09-22 14:41:30 +1000

    Remove globals, we dont need that cruft

--- mocha/common.js
+++
@@ -1,8 +0,0 @@
-global.React = require('react')
-global.sinon = require('sinon')
-global.chai = require('chai')
-global.should = global.chai.should()
-global.assert = global.chai.assert
-global.expect = global.chai.expect
-
-global.chai.use(require('sinon-chai'))
