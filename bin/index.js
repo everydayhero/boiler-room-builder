524038bc1a2ade5bf342af752273e91efc12a2fc
Brad Parker <hi@bradparker.com>
2016-09-27 15:47:02 +1000

    Calling brb now proxies call to `scripts/${action}`
    Where action is the first arg passed to brb

--- bin/index.js
+++ bin/index.js
@@ -1,45 +1,41 @@
 #! /usr/bin/env node
 
-const brb = require('../')
+const { spawn } = require('child_process')
 const { join } = require('path')
 
-const {
-  inputDir = process.env.BRB_INPUT_DIR,
-  outputDir = process.env.BRB_OUTPUT_DIR,
-  basePath = process.env.BRB_BASE_PATH,
-  config: sharedConfigPath,
-  serverConfig: serverConfigPath,
-  clientConfig: clientConfigPath,
-  devConfig: devConfigPath,
-  port = 8080,
-  _
-} = require('yargs').argv
+const isNodeArg = (arg) => (
+  arg === 'debug' ||
+  arg === '--debug' ||
+  arg === '--debug-bk'
+)
 
-const action = _[0]
-const extras = _.slice(1).join(' ')
+const script = process.argv[2]
 
-const clientConfig = clientConfigPath
-  ? require(join(process.cwd(), clientConfigPath))
-  : {}
-const serverConfig = serverConfigPath
-  ? require(join(process.cwd(), serverConfigPath))
-  : {}
-const sharedConfig = sharedConfigPath
-  ? require(join(process.cwd(), sharedConfigPath))
-  : {}
-const devConfig = devConfigPath
-  ? require(join(process.cwd(), devConfigPath))
-  : {}
+let args = [
+  join(__dirname, `../scripts/${script}`)
+]
 
-brb({
-  action,
-  inputDir,
-  outputDir,
-  basePath,
-  serverConfig,
-  clientConfig,
-  sharedConfig,
-  devConfig,
-  port,
-  extras
+process.argv.slice(3).forEach((arg) => {
+  if (isNodeArg(arg)) {
+    args.unshift(arg)
+  } else {
+    args.push(arg)
+  }
 })
+
+const proc = spawn(
+  process.execPath,
+  args,
+  { stdio: 'inherit' }
+)
+
+proc.on('exit', (code, signal) => {
+  process.on('exit', () => {
+    if (signal) {
+      process.kill(process.pid, signal)
+    } else {
+      process.exit(code)
+    }
+  })
+})
+
