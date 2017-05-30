78e76d71254ba1914b6c4d8dcc263fd82a9ee157
Justin Marrington <justin@marrington.net>
2017-05-30 14:47:06 +1000

    [RELEASE] Iterate and set defaultView props to global in tests

--- scripts/test/setup.js
+++ scripts/test/setup.js
@@ -3,3 +3,17 @@ const jsdom = require('jsdom')
 global.document = jsdom.jsdom('<html><body></body></html>')
 global.window = document.defaultView
 global.navigator = window.navigator
+
+// properties must be extracted from a document that was created that does not
+// use a vm context.
+const properties = Object.getOwnPropertyNames(
+  jsdom.jsdom('', { features: { ProcessExternalResources: false } }).defaultView
+)
+
+// Attach all window properties to global, so things like HTMLElement are in
+// scope. See https://github.com/sinonjs/sinon/issues/1377
+properties.forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    global[property] = document.defaultView[property]
+  }
+})
